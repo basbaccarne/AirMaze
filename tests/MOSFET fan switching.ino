26d06e42eeec56cb44d5d0cc894d96ba221e59dd
Bas Baccarne <bastiaan.baccarne@ugent.be>
2024-05-02 21:17:05 +0200

    Update MOSFET fan switching.ino

--- tests/MOSFET fan switching.ino
+++ tests/MOSFET fan switching.ino
@@ -1,15 +1,14 @@
-// simple code to detect the distance and print the reseult in cm to the serial monitor
-// ultrasonic sensor (HC-SR04) trigger on Digital 2, echo on Digital 3
+// simple code to open and close the MOSFET (signal on digital 3)
 
-const int pin = 3;
+const int signalPin = 3;
 
 void setup() {
-  pinMode(pin, OUTPUT); // Define pin as output
+  pinMode(signalPin, OUTPUT); // Define pin as output
 }
 
 void loop() {
-  digitalWrite(pin, HIGH); // Set the pin to HIGH (turns on the motor)
+  digitalWrite(signalPin, HIGH); // Set the pin to HIGH (turns on the motor)
   delay(5000); // Wait for 5 seconds
-  digitalWrite(pin, LOW); // Set the pin to LOW (turns off the motor)
+  digitalWrite(signalPin, LOW); // Set the pin to LOW (turns off the motor)
   delay(5000); // Wait for 5 seconds
 }
